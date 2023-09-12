98beb05aa993c86e92c04d0f103ddd029a122500
Alec Condry <aleccondry@gmail.com>
2023-09-12 15:11:10 -0400

    implement multiply operation

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,6 @@ console.log(`result_add = ${result_add}`);
 
 const result_subtract = calc.subtract(5, 4);	
 console.log(`result_sub = ${result_subtract}`);
+
+const result_mult = calc.multiply(5, 3);	
+console.log(`result_sub = ${result_mult}`);
